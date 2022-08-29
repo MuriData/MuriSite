70e8ea5093411c4a87d8a422b93e848c3b3134fa
Rorical <46294886+Rorical@users.noreply.github.com>
2022-08-29 16:26:07 +0800

    add latex support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,9 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'MuriData',
@@ -25,6 +28,14 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        docs:{
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        blog:{
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        }
       }),
     ],
   ],
@@ -63,6 +74,15 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    stylesheets: [
+      {
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+        type: 'text/css',
+        integrity:
+          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        crossorigin: 'anonymous',
+      },
+    ],
 };
 
 module.exports = config;
